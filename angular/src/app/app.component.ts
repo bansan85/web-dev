c67af9e8267f7a85f7980fa4485bccab51ae024e
Vincent Le Garrec <github@le-garrec.fr>
2024-10-20 21:35:23 +0200

    First implementation of Angular web-demangler

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { WasmLoaderService } from './wasm-loader.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'web-demangler';
+export class AppComponent implements OnInit {
+  demangledName: string[] = [];
+
+  constructor(private wasmLoader: WasmLoaderService) { }
+
+  async ngOnInit() {
+    if (!this.wasmLoader.wasm()) {
+      await this.loadWasmModule();
+    }
+  }
+
+  async loadWasmModule() {
+    while (!this.wasmLoader.wasm()) {
+      await new Promise(resolve => setTimeout(resolve, 100));
+    }
+  }
+
+  onDemangle(mangledName: string) {
+    const wasmInstance = this.wasmLoader.wasm();
+    if (wasmInstance && mangledName) {
+      const lines = mangledName.split('\n');
+      this.demangledName = lines.map(line => wasmInstance.demangle(line.trim()));
+    }
+  }
 }
