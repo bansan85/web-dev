fd7ab7a2b9f8200e197268804d495497e71422ca
Vincent Le Garrec <github@le-garrec.fr>
2024-10-23 15:15:04 +0200

    Fix build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -8,10 +8,10 @@ emcmake cmake -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE="Release"
 cmake --build build --parallel 8
 mkdir -p angular/src/assets
 rm -Rf angular/src/assets/*
-cp build/Release/web_.* angular/src/assets/
+cp build/web_.* angular/src/assets/
 cd angular
 npm install -g npm@latest
 npm install -g @angular/cli
 npm install
 ng build
-
+ng serve --open --host 0.0.0.0
