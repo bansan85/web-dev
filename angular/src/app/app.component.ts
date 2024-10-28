1120961fe3599d9a63fa0024afdccf46fe635f5c
Vincent Le Garrec <github@le-garrec.fr>
2024-10-28 08:12:29 +0100

    Add transition for setting window

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { WasmLoaderDemanglerService } from './wasm-loader-demangler.service';
 import { WasmLoaderFormatterService } from './wasm-loader-formatter.service';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgClass } from '@angular/common';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, NgIf, LucideAngularModule],
+  imports: [NgFor, NgIf, NgClass, LucideAngularModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   demangledName: string[] = [];
+
+  isOpen: boolean = false;
+
+  @ViewChild('dialog', { static: false }) dialogRef!: ElementRef<HTMLDialogElement>;
 
   constructor(private wasmLoaderDemangler: WasmLoaderDemanglerService, private wasmLoaderFormatter: WasmLoaderFormatterService) { }
 
@@ -44,5 +48,22 @@ export class AppComponent implements OnInit {
       const lines = mangledName.split('\n');
       this.demangledName = lines.map(line => wasmFormatterInstance.formatter(wasmDemanglerInstance.web_demangle(line.trim())));
     }
+  }
+
+  openDialog() {
+    this.dialogRef.nativeElement.showModal();
+    setTimeout(() => this.isOpen = true, 0);
+  }
+
+  closeDialog() {
+    const close = (event: TransitionEvent) => {
+      if (event.propertyName === 'opacity') {
+        this.dialogRef.nativeElement.close();
+        this.dialogRef.nativeElement.removeEventListener('transitionend', close);
+      }
+    };
+
+    this.dialogRef.nativeElement.addEventListener('transitionend', close);
+    this.isOpen = false;
   }
 }
