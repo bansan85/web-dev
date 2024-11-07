d04d99386611a68ebac7de3fa41e55f0f89a3ed0
Vincent Le Garrec <github@le-garrec.fr>
2024-11-07 22:17:46 +0100

    Fix build_all.sh script

--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,12 @@
+# Build generator with native llvm to prepare wrapper for clang-format
+cmake -S utils -B build_utils_release -G Ninja -DCMAKE_BUILD_TYPE=Release
+cd build_utils_release
+npm install typescript
+export PATH=$PATH:$(pwd)/node_modules/.bin
+cmake --build . --parallel 8
+cd ..
+
+# Build wasm-assembly wrapper
 cd emsdk
 git pull
 ./emsdk install latest
@@ -8,17 +17,12 @@ emcmake cmake -S . -B build_release -G "Ninja" -DCMAKE_BUILD_TYPE="Release"
 cmake --build build_release --parallel 8
 mkdir -p angular/src/assets
 rm -Rf angular/src/assets/*
-cp build/web_.* angular/src/assets/
+cp build_release/web* angular/src/assets/
+
+# Build Angular project
 cd angular
 npm install -g npm@latest
 npm install -g @angular/cli
 npm install
 ng build
 ng serve --open --host 0.0.0.0
-
-# native llvm for clang-format emscripten wrapper generator
-cmake -S utils -B build_utils_release -G Ninja -DCMAKE_BUILD_TYPE=Release
-cd build_utils_release
-npm install typescript
-export PATH=$PATH:$(pwd)/node_modules/.bin
-cmake --build . --parallel 8
