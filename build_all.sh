619384cb2e2eb93ada1af8ffb794422bf6a4416d
Vincent Le Garrec <github@le-garrec.fr>
2024-11-12 15:00:03 +0100

    Improve buil_all for debug and sanitizers

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,15 @@
-git submodule update --init --recursive
+git submodule update --init --recursive --depth=1
 
 # Build generator with native llvm to prepare wrapper for clang-format
 cmake -S utils -B build_utils_release -G Ninja -DCMAKE_BUILD_TYPE=Release
 cd build_utils_release
+npm install typescript
+export PATH=$PATH:$(pwd)/node_modules/.bin
+cmake --build . --parallel 8
+cd ..
+
+cmake -S utils -B build_utils_debug -G Ninja -DCMAKE_BUILD_TYPE=Debug
+cd build_utils_debug
 npm install typescript
 export PATH=$PATH:$(pwd)/node_modules/.bin
 cmake --build . --parallel 8
@@ -15,11 +22,18 @@ git pull
 ./emsdk activate latest
 source ./emsdk_env.sh
 cd ..
+
 emcmake cmake -S . -B build_release -G "Ninja" -DCMAKE_BUILD_TYPE="Release"
-cmake --build build_release --parallel 8
+cmake --build build_release --parallel $(nproc --all)
 mkdir -p angular/src/assets
 rm -f angular/src/assets/*
 cp build_release/web* angular/src/assets/
+
+emcmake cmake -S . -B build_debug -G "Ninja" -DCMAKE_BUILD_TYPE="Debug" -DWITH_SANITIZE_ADDRESS=ON -DWITH_SANITIZE_UNDEFINED=ON
+cmake --build build_debug --parallel $(nproc --all)
+mkdir -p angular/src/assets
+rm -f angular/src/assets/*
+cp build_debug/web* angular/src/assets/
 
 # Build Angular project
 cd angular
@@ -28,3 +42,4 @@ npm install -g @angular/cli
 npm install
 ng build
 ng serve --open --host 0.0.0.0
+ng test --browsers=ChromeHeadless --watch=false
