b4592773a24d98514838bb066c7d0d1a60efe494
Vincent Le Garrec <github@le-garrec.fr>
2024-12-01 23:43:10 +0100

    Move spinner loading in a dedicated component

---
+++ angular/src/app/templates/spinner-loading.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-spinner-loading',
+  standalone: true,
+  imports: [LucideAngularModule],
+  templateUrl: './spinner-loading.component.html',
+  styleUrl: './spinner-loading.component.css',
+})
+export class SpinnerLoadingComponent {
+  @Input({ required: true }) size!: number;
+}
