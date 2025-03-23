39625835fd71dd6d6aaa5beadbc5991602ae57a0
Vincent Le Garrec <github@le-garrec.fr>
2025-03-23 12:11:35 +0100

    Add Angular pdf component

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { AppDemanglerComponent } from './apps/demangler.component';
 import { AppFormatterComponent } from './apps/formatter.component';
 import { AppLightenComponent } from './apps/lighten.component';
 import { AppNamingStyleComponent } from './apps/naming-style.component';
+import { MainPdfComponent } from './apps/main-pdf.component';
 
 export const routes: Routes = [
   { path: 'demangler', component: AppDemanglerComponent },
@@ -12,4 +13,5 @@ export const routes: Routes = [
   { path: 'naming-style', component: AppNamingStyleComponent },
   { path: 'lighten', component: AppLightenComponent },
   { path: 'clang-format-config-migrate', component: ClangFormatConfigMigrateComponent },
+  { path: 'pdf', component: MainPdfComponent },
 ];
